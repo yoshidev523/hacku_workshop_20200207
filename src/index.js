665b7474a1df151fcb620f1d8bb54e61c6eea86e
Yoshiki Honda <yoshidev523@gmail.com>
2020-02-03 23:01:43 +0900

    完成

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+for (var num = 9; num > 0; num--) {
+  var elm = document.createElement("button");
+  elm.innerHTML = num;
+  elm.setAttribute("id", num);
+  elm.setAttribute("class", "circle");
+  var function_name = "remove(" + num + ")";
+  elm.setAttribute("onclick", function_name);
+  document.getElementById("main").appendChild(elm);
+
+  var left_pos = 10;
+  var top_pos = 100;
+  left_pos = left_pos + Math.floor(Math.random() * 400);
+  top_pos = top_pos + Math.floor(Math.random() * 600);
+  document.getElementById(num).style.left = "" + left_pos + "px";
+  document.getElementById(num).style.top = "" + top_pos + "px";
+}
+
+var next_num = 1;
+document.remove = function(id) {
+  if (id === next_num) {
+    document.getElementById("main").removeChild(document.getElementById(id));
+    next_num = next_num + 1;
+  }
+};
